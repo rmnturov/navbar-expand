01c04a41cc68ced560bd0e310df4d09b526950db
Roman Turov <31649219+rmnturov@users.noreply.github.com>
2025-10-02 23:54:00 +0700

    fix references

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '@koobiq/design-tokens/web/css-tokens.css'
+import '@koobiq/design-tokens/web/css-tokens-light.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
