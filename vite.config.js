ad31ecae694f63dea4f9b460ca3579d54f0dd635
Roman Turov <31649219+rmnturov@users.noreply.github.com>
2025-10-06 13:21:07 +0700

    404 and URLs

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
     open: true
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        '404': './404.html'
+      }
+    }
   }
 })
